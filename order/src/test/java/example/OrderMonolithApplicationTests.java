4a7d9e324e91f5a58c6111a23b98a080ae3c8a0f
philipz <philipzheng@gmail.com>
2024-02-25 17:25:54 +0800

    feat: add modulith docs

--- order/src/test/java/example/OrderMonolithApplicationTests.java
+++ order/src/test/java/example/OrderMonolithApplicationTests.java
@@ -2,6 +2,8 @@ package example;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.modulith.core.ApplicationModules;
+import org.springframework.modulith.docs.Documenter;
 
 @SpringBootTest
 class OrderMonolithApplicationTests {
@@ -9,4 +11,15 @@ class OrderMonolithApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+	
+	ApplicationModules modules = ApplicationModules
+       .of(OrderMonolithApplication.class);
+ 
+    @Test
+    void writeDocumentationSnippets() {
+       new Documenter(modules)
+              .writeModuleCanvases()
+              .writeModulesAsPlantUml()
+              .writeIndividualModulesAsPlantUml();
+    }
 }
